85ae68d9a471ba34e407ae84d42aa53aaefa7a8f
shcharina <arina.shchelkanova@gmail.com>
2026-04-08 21:30:22 +0700

    ReferenceController переделан под получение информации от Менеджера

--- InternshipManager.Api/Controllers/v1/ReferenceController.cs
+++ InternshipManager.Api/Controllers/v1/ReferenceController.cs
@@ -1,23 +1,14 @@
 using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
-using InternshipManager.Api.Data;
-using InternshipManager.Api.Enums;
-
-namespace InternshipManager.Api.Controllers;
-
-[ApiController]
-[Asp.Versioning.ApiVersion("1.0")]
-[Route("api/v{version:apiVersion}/[controller]")]
+using InternshipManager.Api.Services;
 
 public class ReferenceController : ControllerBase
 {
-    private readonly AppDbContext _context;
-
-    public ReferenceController(AppDbContext context)
+    private readonly ManagerApiClient _managerApi;
+    public ReferenceController(ManagerApiClient managerApi)
     {
-        _context = context;
+        _managerApi = managerApi;
     }
 
     // GET api/v1/Reference/specializations
@@ -26,14 +17,7 @@ public class ReferenceController : ControllerBase
 
     public async Task<IActionResult> GetSpecializations()
     {
-        var specializations = await _context.Specializations
-            .Select(s => new
-            {
-                id = s.IdSpecialization,
-                name = s.Name
-            })
-            .ToListAsync();
-
+        var specializations = await _managerApi.GetSpecializationsAsync();
         return Ok(specializations);
     }
 
@@ -43,46 +27,21 @@ public class ReferenceController : ControllerBase
 
     public async Task<IActionResult> GetDepartments()
     {
-        var departments = await _context.Departments
-            .Select(d => new
-            {
-                id = d.IdDepartment,
-                name = d.Name
-            })
-            .ToListAsync();
-
+        var departments = await _managerApi.GetDepartmentsAsync();
         return Ok(departments);
     }
 
     // GET api/v1/Reference/addresses
 
-    // Можно фильтровать по подразделению
-
     [HttpGet("addresses")]
-
-    public async Task<IActionResult> GetAddresses([FromQuery] int? departmentId = null)
+    public async Task<IActionResult> GetAddresses(
+        [FromQuery] int? departmentId = null)
     {
-        var query = _context.Addresses.AsQueryable();
-
-        if (departmentId.HasValue)
-            query = query.Where(a => a.IdDepartment == departmentId.Value);
-
-        var addresses = await query
-            .Select(a => new
-            {
-                id = a.IdAddress,
-                fullAddress = a.FullAddress,
-                city = a.City,
-                idDepartment = a.IdDepartment
-            })
-            .ToListAsync();
-
+        var addresses = await _managerApi.GetAddressesAsync(departmentId);
         return Ok(addresses);
     }
 
     // GET api/v1/Reference/practice-formats
-
-    // Enum как список
 
     [HttpGet("practice-formats")]
 
@@ -94,9 +53,7 @@ public class ReferenceController : ControllerBase
             new { id = 2, name = "Дистанционная" },
             new { id = 3, name = "Гибридная" }
         };
-
         return Ok(formats);
-
     }
 
     // GET api/v1/Reference/scheduled-practices
@@ -105,17 +62,7 @@ public class ReferenceController : ControllerBase
 
     public async Task<IActionResult> GetScheduledPractices()
     {
-        var practices = await _context.ScheduledPractices
-            .Select(sp => new
-            {
-                id = sp.IdScheduledPractice,
-                idSpecialization = sp.IdSpecialization,
-                startDate = sp.StartDate,
-                endDate = sp.EndDate
-            })
-            .ToListAsync();
-
+        var practices = await _managerApi.GetScheduledPracticesAsync();
         return Ok(practices);
     }
-
 }
